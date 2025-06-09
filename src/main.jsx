ab1f513fe004a6ca0aedfbef8c1d7bf1177992a0
defeger <defflox@gmail.com>
2025-06-10 01:14:21 +0400

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import { Router } from 'react-router-dom';
+import router from './routers/Router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={Router} />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
