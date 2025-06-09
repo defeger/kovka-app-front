28ecc1e8108e123b345b4b492dfc8b397ee0400d
defeger <97786925+defeger@users.noreply.github.com>
2025-06-10 02:20:18 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   preview: {
     host: true,
     port: 4173,
-    allowedHosts: ['https://kovka-app-front.onrender.com/']
+    allowedHosts: ['kovka-app-front.onrender.com']
   }
 })
