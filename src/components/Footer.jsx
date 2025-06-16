e049182dc47fafcde316c1f5373c425fa2482bcb
defeger <97786925+defeger@users.noreply.github.com>
2025-06-16 22:26:10 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link';
-import fire from "../media/fire.svg"
+import fire from "../media/kovka2.svg"
 
 const Footer = () => {
   return (
