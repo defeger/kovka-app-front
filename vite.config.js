49f55df6826dc384c335c9412151b35532c13467
defeger <97786925+defeger@users.noreply.github.com>
2025-08-09 15:35:58 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     preview: {
-    allowedHosts: ['kovka-app-front.onrender.com'],
+    allowedHosts: ['https://kovka-app-front.onrender.com/'],
   },
 })
